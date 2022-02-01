81481ca86f53f23ebad8f2bb309180df96f09fdc
aouataf <aouatefd@yahoo.com>
2022-02-01 12:52:30 +0100

    changes

--- sortSentence.py
+++ sortSentence.py
@@ -1,14 +1,32 @@
+"""
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
+
+A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+For example, the sentence "This is a sentence" can be shuffled as "sentence4 a3 is2 This1" or "is2 sentence4 This1 a3".
+
+"""
+
 class Solution:
     def sortSentence(self, s:str)-> str:
-        s=[word for word in s.split()]
-        print(s)
+        if type(s)==str:
+            s=[word for word in s.split()]
+        
+        if len(s)<=1:
+            return ''.join(e[:-1] for e in s)
+        """
+        divide
+        """
+        
         
         mid=len(s)//2
         left=s[:mid]
         right=s[mid:]
-        self.sortSentence(left.split())
-        self.sortSentence(right.split())
-        
+        self.sortSentence(left)
+        self.sortSentence(right)
+        """
+        merge
+        """
         i=j=k=0
         while i<len(left) and j<len(right):
             if left[i][-1]<right[j][-1]:
@@ -27,12 +45,13 @@ class Solution:
             s[k]=left[i]
             i+=1
             k+=1
-              
+        """
+        if right sublist is longer than the left one 
+        """       
         while j<len(right):
             s[k]=right[j]
             j+=1
             k+=1
-        print(type(s))
-        return s
-s = "is2 sentence4 This1 a3"
-print(Solution.sortSentence(s,"is2 sentence4 This1 a3"))
+        
+        return ' '.join(e[:-1] for e in s)
+        
